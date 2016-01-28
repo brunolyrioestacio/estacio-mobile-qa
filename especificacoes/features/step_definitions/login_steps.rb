fe6bd41dc635d23858cd77d8130151254a4d5566
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-01-28 12:25:28 -0200

    Commun steps defined

--- especificacoes/features/step_definitions/login_steps.rb
+++ especificacoes/features/step_definitions/login_steps.rb
@@ -1,5 +1,35 @@
 ######### DADO #########
 
+Dado(/^que estou na tela de Login$/) do
+end
+
 ######### QUANDO #########
 
+Quando(/^tocar no botão de login$/) do
+end
+
+########  E  ##########
+Quando(/^não preenchi o campo de matrícula$/) do
+end
+
+Quando(/^não preenchi o campo de senha$/) do
+end
+
+Quando(/^preenchi o campo de senha$/) do
+end
+
+Quando(/^preenchi o campo de matrícula$/) do
+end
+
+Quando (/^preenchi o campo de matrícula incorretamente$/) do
+end
+
+Quando (/^preenchi o campo de senha incorretamente$/) do
+end
+
 ######### ENTãO #########
+Então (/^devo ver uma mensagem de erro$/) do
+end
+
+Então (/^devo ver a tela inicial$/) do
+end
