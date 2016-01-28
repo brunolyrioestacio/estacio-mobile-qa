dd5fe0551e9490f579ac44c2d467e9f70729e45f
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-01-28 12:10:31 -0200

    iOS steps

--- especificacoes/features/ios/step_definitions/login_steps.rb
+++ especificacoes/features/ios/step_definitions/login_steps.rb
@@ -1,5 +1,16 @@
 ######### DADO #########
+Dado(/^que estou na tela OnBoarding$/) do
+end
 
 ######### QUANDO #########
 
+###########E############
+E(/^devo percorrer o tutorial/) do
+end
+
 ######### ENTãO #########
+Então(/^devo aceitar a permissão de push/) do
+end
+
+Então(/^ devo negar a permissão de push/) do
+end
