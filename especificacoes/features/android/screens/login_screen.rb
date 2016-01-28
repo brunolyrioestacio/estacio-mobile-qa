886c7572b834a863fc88bcbcae8008d25b7cacf0
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-01-28 15:19:58 -0200

    android login screen created

--- especificacoes/features/android/screens/login_screen.rb
+++ especificacoes/features/android/screens/login_screen.rb
@@ -1,13 +1,26 @@
 class LoginScreen < AndroidScreenBase
   # Identificador da tela
-  # trait(:trait)                 { "* id:'#{layout_name}'" }
+
+   trait(:trait)                 { "* id:'#{layout_name}'" }
 
   # Declare todos os elementos da tela
-  # element(:layout_name)         { 'insert_layout_identificator' }
-  # element(:button)              { 'insert_button_identificator' }
+  element(:layout_name)         { 'login_activity' }
+  element(:button)              { '' }
+  element(:campo_matricula)           {''}
+  element(:campo_senha)               {''}
 
   # Declare todas as acoes da tela
-  # action(:touch_button) {
-  #   touch("* id:'#{button}'")
-  # }
+
+   action(:tocar_botao_login) {
+     touch("* id:'#{button}'")
+   }
+  def matricula(matricula)
+    enter matricula, campo_matricula
+  end
+
+  def senha (senha)
+    enter senha, campo_senha
+  end
+
+
 end
