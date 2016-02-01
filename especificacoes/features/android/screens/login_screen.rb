5a02c6417bcc835559e47476f32cfb03bca27508
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-02-01 17:30:13 -0200

    Android Login Screen updated

--- especificacoes/features/android/screens/login_screen.rb
+++ especificacoes/features/android/screens/login_screen.rb
@@ -5,22 +5,25 @@ class LoginScreen < AndroidScreenBase
 
   # Declare todos os elementos da tela
   element(:layout_name)         { 'login_activity' }
-  element(:button)              { 'login_btn' }
+  element(:login_button)              { 'login_btn' }
   element(:campo_matricula)     { 'input_registration' }
   element(:campo_senha)         { 'input_password' }
 
   # Declare todas as acoes da tela
 
    action(:tocar_botao_login) {
-     touch("* id:'#{button}'")
+     touch("* id:'#{login_button}'")
    }
-  def matricula(matricula)
+  def enter_matricula(matricula)
     enter matricula, campo_matricula
   end
 
-  def senha (senha)
+  def enter_senha (senha)
     enter senha, campo_senha
   end
 
+  def btn_disabled?
+    query("* id:'#{login_button}' enabled:'false'")
+  end
 
 end
