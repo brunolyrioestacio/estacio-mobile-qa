9c04c386765cbbc5125ff445dc048e32255fd3fb
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-02-04 18:13:47 -0200

    iOS home screen new elements add

--- especificacoes/features/ios/screens/home_screen.rb
+++ especificacoes/features/ios/screens/home_screen.rb
@@ -3,10 +3,24 @@ class HomeScreen < IOSScreenBase
    trait(:trait)                 { "* marked:'#{layout_name}'" }
 
   # Declare todos os elementos da tela
-   element(:layout_name)         { 'homeView' }
-   element(:entendi_button)      {'OK, entendi'}
+   element(:layout_name)           { 'homeView' }
+   element(:entendi_button)        {'OK, entendi'}
+   element(:outras_funcoes_button) {'Outras Funções'}
+   element(:sair_button)           {'Sair'}
 
    action(:tocar_botao_entendi) {
      touch("* marked:'#{entendi_button}'")[1]
    }
+
+   action(:tocar_botao_outras_funcoes) {
+     touch("* marked:'#{outras_funcoes_button}'")
+   }
+   action(:tocar_botao_sair) {
+     touch("* marked:'#{sair_button}'")
+   }
+
+   def alerta_logout_is_visible?
+     is_on_page? "Deseja mesmo sair?"
+   end
+
 end
