9e5501a410f591d7402374892c32d4d52a49396f
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-02-05 17:54:51 -0200

    iOS banner scenario created and implemented

--- especificacoes/features/ios/features/home.feature
+++ especificacoes/features/ios/features/home.feature
@@ -2,7 +2,18 @@
 Funcionalidade: Home
 Com essa funcionalidade é possível validar os banners da tela principal
 
-@testa
-Cenário: Vizualização dos banners
+@entra
+Cenário: Login
+  Dado que estou na tela de Login
+  E preenchi o campo de matrícula
+  E preenchi o campo de senha
+  Quando tocar no botão de login
+  Então devo ver a tela inicial
+
+@banner
+Cenário: Validação do banner
   Dado que estou na tela inicial
-  Então devo ver os banners
+  E toquei no banner
+  E estou na tela de detalhes do banner
+  Quando tocar na mesma
+  Então devo ver um alerta de redirecionamento para uma página externa
--- especificacoes/features/ios/step_definitions/home_steps.rb
+++ especificacoes/features/ios/step_definitions/home_steps.rb
@@ -1,15 +1,20 @@
 ######### DADO #########
-Dado(/^que estou na tela inicial$/) do
-  @pageHome = page(HomeScreen).await(timeout:5)
-end
-
 ######### QUANDO #########
-
+Quando(/^tocar na mesma$/) do
+  @pageHome = page(HomeScreen).await(timeout:5)
+  @pageHome.tocar_detalhe_banner
+end
 
 ###########E############
 
-######### ENTãO #########
+Quando(/^toquei no banner$/) do
+  @pageHome.tocar_banner
+end
 
-Então(/^devo ver os banners$/) do
-  @pageHome.banner_is_visible
+Quando(/^estou na tela de detalhes do banner$/) do
+  @pageHome.estou_no_detalhe_do_banner
+end
+######### ENTãO #########
+Então(/^devo ver um alerta de redirecionamento para uma página externa$/) do
+  @pageHome.is_on_page? "Você será direcionado para um site externo. Deseja continuar?"
 end
