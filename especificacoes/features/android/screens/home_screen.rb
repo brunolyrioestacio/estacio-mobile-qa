5ef7add80511ff33807b0090cf6d9c69a782a2ec
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-02-11 10:23:31 -0200

    android home screen add logout elements

--- especificacoes/features/android/screens/home_screen.rb
+++ especificacoes/features/android/screens/home_screen.rb
@@ -1,11 +1,24 @@
 class HomeScreen < AndroidScreenBase
   # Identificador da tela
 
-   trait(:trait)                 { "* id:'#{layout_name}'" }
+  trait(:trait)                 { "* id:'#{layout_name}'" }
 
   # Declare todos os elementos da tela
-  element(:layout_name)         { 'home_view' }
+  element(:layout_name)           { 'home_view' }
+  element(:sair_button)           {'log_out'}
+  element(:confirmar_sair_button) {'custom_alert_second_btn'}
 
   # Declare todas as acoes da tela
+  action(:tocar_botao_sair) {
+    touch("* marked:'#{sair_button}'")
+  }
+
+  action(:tocar_botao_confirmar_sair){
+    touch("* marked:'#{confirmar_sair_button}'")
+  }
+
+  def alerta_logout_is_visible?
+    is_on_page? "Deseja mesmo sair?"
+  end
 
 end
