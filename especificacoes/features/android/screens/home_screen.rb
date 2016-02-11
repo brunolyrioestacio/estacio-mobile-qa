101996a8b391310053b88c0fa7c8d4a13f162a95
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-02-11 16:44:10 -0200

    banner elements add to android home screen

--- especificacoes/features/android/screens/home_screen.rb
+++ especificacoes/features/android/screens/home_screen.rb
@@ -7,6 +7,8 @@ class HomeScreen < AndroidScreenBase
   element(:layout_name)           { 'home_view' }
   element(:sair_button)           {'log_out'}
   element(:confirmar_sair_button) {'custom_alert_second_btn'}
+  element(:banner)                {'carousel_view_pager'}
+
 
   # Declare todas as acoes da tela
   action(:tocar_botao_sair) {
@@ -15,6 +17,10 @@ class HomeScreen < AndroidScreenBase
 
   action(:tocar_botao_confirmar_sair){
     touch("* marked:'#{confirmar_sair_button}'")
+  }
+
+  action(:tocar_banner) {
+    touch("* marked:'#{banner}'")
   }
 
   def alerta_logout_is_visible?
