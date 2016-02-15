25d8949aee5172fdbadd526fee1eaf54d74341dd
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-02-15 19:36:13 -0200

    updating digital card screen with view id

--- especificacoes/features/ios/screens/carteira_digital_screen.rb
+++ especificacoes/features/ios/screens/carteira_digital_screen.rb
@@ -3,7 +3,7 @@ class CarteiraDigitalScreen < IOSScreenBase
    trait(:trait)                 { "* marked:'#{layout_name}'" }
 
   # Declare todos os elementos da tela
-   element(:layout_name)         { '' }
+   element(:layout_name)         { 'digitalCardView' }
 
    def valida_dados_na_tela(matricula,cpf)
      is_on_page? matricula
