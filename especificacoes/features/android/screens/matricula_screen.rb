a9c125c0bb029f0837754ee9d9da9651e4394244
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-02-15 17:04:20 -0200

    android forget user registration screens created

--- especificacoes/features/android/screens/matricula_screen.rb
+++ especificacoes/features/android/screens/matricula_screen.rb
@@ -3,8 +3,26 @@ class MatriculaScreen < AndroidScreenBase
 
   trait(:trait)                 { "* id:'#{layout_name}'" }
   # Declare todos os elementos da tela
+  element(:campo_identidade)                { ''}
+  element(:campo_nascimento)                { ''}
+  element(:enviar_button)                   { 'Continuar'}
 
   # Declare todas as acoes da tela
 
+  def enter_identidade(identidade)
+    enter identidade, campo_identidade
+  end
+
+  def enter_nascimento(nascimento)
+    enter nascimento, campo_nascimento
+  end
+
+  def btn_disabled?
+    query("* id:'#{enviar_button}'", :isEnabled)[0].equal? 0
+  end
+
+  def tocar_botao_continuar
+     touch("* id:'#{enviar_button}'")
+  end
 
 end
