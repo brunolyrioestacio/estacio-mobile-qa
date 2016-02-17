c6e70dd30d5851b30980a7f4938b96ee3ea0d25f
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-02-17 17:21:21 -0200

    android student card view id added

--- especificacoes/features/android/screens/carteira_digital_screen.rb
+++ especificacoes/features/android/screens/carteira_digital_screen.rb
@@ -3,7 +3,7 @@ class CarteiraDigitalScreen < AndroidScreenBase
    trait(:trait)                 { "* id:'#{layout_name}'" }
 
   # Declare todos os elementos da tela
-   element(:layout_name)         { 'student_card_view' }
+   element(:layout_name)         { 'student_card_activity' }
 
    def valida_dados_na_tela(matricula,cpf)
      is_on_page? matricula
