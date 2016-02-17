47c5429d9442b0f643de2b2157de936f3ec8fb37
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-02-17 03:07:48 -0200

    Sleeping before touch identity field

--- especificacoes/features/android/screens/matricula_screen.rb
+++ especificacoes/features/android/screens/matricula_screen.rb
@@ -4,13 +4,14 @@ class MatriculaScreen < AndroidScreenBase
   trait(:trait)                 { "* id:'#{layout_name}'" }
   # Declare todos os elementos da tela
   element(:layout_name)                     { 'forgot_registration_activity' }
-  element(:campo_identidade)                { 'number_id'}
+  element(:campo_identidade)                { 'number_identity'}
   element(:campo_nascimento)                { 'date_of_birth'}
-  element(:enviar_button)                   { 'Continuar'}
+  element(:enviar_button)                   { 'continue_btn'}
 
   # Declare todas as acoes da tela
 
   def enter_identidade(identidade)
+    sleep 1
     enter identidade, campo_identidade
   end
 
