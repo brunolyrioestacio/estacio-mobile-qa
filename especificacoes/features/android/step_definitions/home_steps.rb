184681e9712bbca455eacb4d3dd999aae8ebd83b
Ingrid Andrade <ingrid.andrade@concretesolutions.com.br>
2016-02-22 12:00:44 -0300

    Excluding ambigus steps on home steps

--- especificacoes/features/android/step_definitions/home_steps.rb
+++ especificacoes/features/android/step_definitions/home_steps.rb
@@ -5,13 +5,6 @@
 
 ###########E############
 
-Quando(/^vi um alerta com o aviso de logout$/) do
-  @pageHome.alerta_logout_is_visible?
-end
-
-Quando(/^toquei no botão de Sair do alerta$/) do
-  @pageHome.tocar_botao_confirmar_sair
-end
 
 Quando(/^toquei no botão de Manual do Aluno$/) do
   @pageHome.tocar_botao_manual_aluno
@@ -20,10 +13,6 @@ end
 
 
 ######### ENTãO #########
-
-Então(/^devo estar na tela de Login$/) do
-  @pageLogin = page(LoginScreen).await(timeout:5)
-end
 
 Então (/^devo vizualizar o Manual do aluno$/) do
   @pageHome.is_on_page? "Manual do Aluno"
