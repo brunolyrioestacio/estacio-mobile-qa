bfade6982418f660f0a437a61025bd4dff0128a8
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-02-29 19:03:04 -0300

    updating variable name at Notas steps

--- especificacoes/features/step_definitions/notas_steps.rb
+++ especificacoes/features/step_definitions/notas_steps.rb
@@ -1,6 +1,6 @@
 ######### DADO #########
 Dado(/^que estou na tela de Notas$/) do
-  @pageNotas = page(NotaScreen).await(timeout:5)
+  @pageNotas = page(NotasScreen).await(timeout:5)
 end
 
 Dado(/^que preenchi o campo de matrícula com uma matricula do período vigente$/) do
@@ -19,13 +19,13 @@ end
 
 ######### ENTãO #########
 Então (/^devo vizualizar as notas da disciplina de Estrutura de Dados$/) do
-  @pageNota.grades_is_on_page?
+  @pageNotas.grades_is_on_page?
 end
 
 Então (/^devo ver uma mensagem de aviso sobre a falta de notas lançadas$/) do
-  @pageNota.validate_no_grades
+  @pageNotas.validate_no_grades
 end
 
 Então (/^devo ver uma mensagem de aviso sobre a falta de notas lançadas devido ao aluno não estar matriculado$/) do
-  @pageNota.validate_no_class_registration
+  @pageNotas.validate_no_class_registration
 end
