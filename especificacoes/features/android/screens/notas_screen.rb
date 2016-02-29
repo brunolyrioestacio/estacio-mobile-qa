bfe29e2e9bf89133d33429f56c863d703600286a
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-02-29 18:17:40 -0300

    Notas android screen created

--- especificacoes/features/android/screens/notas_screen.rb
+++ especificacoes/features/android/screens/notas_screen.rb
@@ -1,13 +1,26 @@
 class NotasScreen < AndroidScreenBase
-  # Identificador da tela
-  # trait(:trait)                 { "* id:'#{layout_name}'" }
 
-  # Declare todos os elementos da tela
-  # element(:layout_name)         { 'insert_layout_identificator' }
-  # element(:button)              { 'insert_button_identificator' }
+    # Declare todos os elementos da tela
+    element(:layout_name)         { '' }
+    # element(:button)              { 'insert_button_identificator' }
 
-  # Declare todas as acoes da tela
-  # action(:touch_button) {
-  #   touch("* id:'#{button}'")
-  # }
+    # Declare todas as acoes da tela
+    # action(:touch_button) {
+    #   touch("* marked:'#{button}'")
+    # }
+
+    def grades_is_on_page
+      is_on_page? "Estrutura de dados"
+      is_on_page? "" #AV1#
+      is_on_page? "" #AV2#
+      is_on_page? "" #AV3#
+    end
+
+    def validate_no_grades
+      is_on_page? ""
+    end
+
+    def validate_no_class_registration
+      is_on_page? ""
+    end
 end
