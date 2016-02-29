2fee51eb49c07c71d2b7f32aa1c754f17fe9fb4b
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-02-29 18:37:56 -0300

    Notas android screen updated with status and assertions

--- especificacoes/features/android/screens/notas_screen.rb
+++ especificacoes/features/android/screens/notas_screen.rb
@@ -1,7 +1,7 @@
 class NotasScreen < AndroidScreenBase
 
     # Declare todos os elementos da tela
-    element(:layout_name)         { '' }
+    element(:layout_name)         { 'student_grades_view' }
     # element(:button)              { 'insert_button_identificator' }
 
     # Declare todas as acoes da tela
@@ -11,16 +11,16 @@ class NotasScreen < AndroidScreenBase
 
     def grades_is_on_page
       is_on_page? "Estrutura de dados"
-      is_on_page? "" #AV1#
-      is_on_page? "" #AV2#
-      is_on_page? "" #AV3#
+      is_on_page? "3.5" #AV1#
+      is_on_page? "9.5" #AV2#
+      is_on_page? "7.9" #AV3#
     end
 
     def validate_no_grades
-      is_on_page? ""
+      is_on_page? "Aluno não está matriculado em nenhum Período Acadêmico vigente."
     end
 
     def validate_no_class_registration
-      is_on_page? ""
+      is_on_page? "Não existem lançamentos de notas para você no Período Acadêmico vigente!"
     end
 end
