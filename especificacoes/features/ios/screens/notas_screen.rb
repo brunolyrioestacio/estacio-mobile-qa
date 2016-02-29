887bf51394a8c6127e17de293893ab99083cf1aa
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-02-29 19:25:29 -0300

    updating ios notas screen assertions

--- especificacoes/features/ios/screens/notas_screen.rb
+++ especificacoes/features/ios/screens/notas_screen.rb
@@ -4,25 +4,19 @@ class NotasScreen < IOSScreenBase
 
   # Declare todos os elementos da tela
   element(:layout_name)         { '' }
-  # element(:button)              { 'insert_button_identificator' }
-
-  # Declare todas as acoes da tela
-  # action(:touch_button) {
-  #   touch("* marked:'#{button}'")
-  # }
 
   def grades_is_on_page
     is_on_page? "Estrutura de dados"
-    is_on_page? "" #AV1#
-    is_on_page? "" #AV2#
-    is_on_page? "" #AV3#
+    is_on_page? "3.5" #AV1#
+    is_on_page? "9.5" #AV2#
+    is_on_page? "7.9" #AV3#
   end
 
   def validate_no_grades
-    is_on_page? ""
+    is_on_page? "Aluno não está matriculado em nenhum Período Acadêmico vigente."
   end
 
   def validate_no_class_registration
-    is_on_page? ""
+    is_on_page? "Não existem lançamentos de notas para você no Período Acadêmico vigente!"
   end
 end
