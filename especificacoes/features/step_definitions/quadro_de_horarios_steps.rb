eb0cb4d9a1e2dce8269ae0569a22dcc155d28005
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-03-01 19:34:50 -0300

    android student schedule steps updated and implemented

--- especificacoes/features/step_definitions/quadro_de_horarios_steps.rb
+++ especificacoes/features/step_definitions/quadro_de_horarios_steps.rb
@@ -1,5 +1,22 @@
 ######### DADO #########
-
+Dado(/^que estou na tela de quadro de horarios$/) do
+  @pageQuadroHorario = page(QuadroDeHorariosScreen).await(timeout:5)
+end
+Dado(/^que preenchi o campo de matrícula com uma matricula que não possui disciplinas escolhidas$/) do
+  # @pageLogin.enter_matricula()
+  pending
+end
 ######### QUANDO #########
 
 ######### ENTãO #########
+Então(/^devo vizualizar o horario da disciplina de Modelagem de Sistemas$/) do
+  @pageQuadroHorario.schedule_is_on_page?
+end
+
+Então(/^devo ver uma mensagem de aviso sobre a indisponibilidade de consulta ao quadro de horario$/) do
+  @pageQuadroHorario.validate_no_schedule_available
+end
+
+Então(/^devo ver uma mensagem de aviso sobre a falta de horarios devido ao aluno não estar matriculado em nenhuma disciplina$/) do
+  @pageQuadroHorario.validate_no_class_registration
+end
