19775ffaf55a91b554223f89aa789e2d5997d006
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-03-01 15:22:51 -0300

    Quadro de Horarios feature created with screen and steps

---
+++ especificacoes/features/ios/features/quadro_de_horarios.feature
@@ -0,0 +1,8 @@
+# language: pt
+Funcionalidade: Quadro De Horarios 
+
+  Contexto:
+    # Insira os passos
+
+  Cenário: Primeiro Cenário
+    # Insira os passos
---
+++ especificacoes/features/ios/screens/quadro_de_horarios_screen.rb
@@ -0,0 +1,13 @@
+class QuadroDeHorariosScreen < IOSScreenBase
+  # Identificador da tela
+  # trait(:trait)                 { "* marked:'#{layout_name}'" }
+
+  # Declare todos os elementos da tela
+  # element(:layout_name)         { 'insert_layout_identificator' }
+  # element(:button)              { 'insert_button_identificator' }
+
+  # Declare todas as acoes da tela
+  # action(:touch_button) {
+  #   touch("* marked:'#{button}'")
+  # }
+end
