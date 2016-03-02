977e01340f9d165a2b12378436094d1b7d0c6564
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-03-02 01:59:09 -0300

    creating new methods at android attendance screen

--- especificacoes/features/android/screens/faltas_screen.rb
+++ especificacoes/features/android/screens/faltas_screen.rb
@@ -6,8 +6,19 @@ class FaltasScreen < AndroidScreenBase
   element(:layout_name)         { 'activity_student_frequency' }
   # element(:button)              { 'insert_button_identificator' }
 
-  # Declare all actions of this screen
-  # action(:touch_button) {
-  #   touch("* id:'#{button}'")
-  # }
+  def attendance_is_on_page?
+    is_on_page? "ARQUITETURA DE SISTEMAS DISTRIBUIDOS"
+    is_on_page? "Total de faltas"
+    is_on_page? "Limite de faltas permitidas"
+  end
+
+  def attendance_is_not_on_page?
+    is_on_page?("Total de faltas",false)
+    is_on_page?("Limite de faltas permitidas",false)
+  end
+
+  def validate_no_attendance_message
+    is_on_page? "Não existe frequência para o aluno!"
+  end
+
 end
