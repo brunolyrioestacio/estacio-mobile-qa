56ef5a6d46316154492b0ac50a4107d3b4862419
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-03-02 02:06:39 -0300

    updating ios attendance screen

--- especificacoes/features/ios/screens/faltas_screen.rb
+++ especificacoes/features/ios/screens/faltas_screen.rb
@@ -2,12 +2,20 @@ class FaltasScreen < IOSScreenBase
   # The screen identificator
   # trait(:trait)                 { "* marked:'#{layout_name}'" }
 
-  # Declare all the elements of this screen
-  # element(:layout_name)         { 'insert_layout_identificator' }
-  # element(:button)              { 'insert_button_identificator' }
+  element(:layout_name)         { '' }
 
-  # Declare all actions of this screen
-  # action(:touch_button) {
-  #   touch("* marked:'#{button}'")
-  # }
+  def attendance_is_on_page?
+    is_on_page? "ARQUITETURA DE SISTEMAS DISTRIBUIDOS"
+    is_on_page? "% Faltas"
+    is_on_page? "44h"
+  end
+
+  def attendance_is_not_on_page?
+    is_on_page?("Total de faltas",false)
+    is_on_page?("Limite de faltas permitidas",false)
+  end
+
+  def validate_no_attendance_message
+    is_on_page? "Não existe frequência para o aluno!"
+  end
 end
