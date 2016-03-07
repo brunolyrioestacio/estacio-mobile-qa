6480002da64dfb241dac0ba1c36baeabcc077758
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-03-07 20:42:22 -0300

    adding new validation mathods at time table screen

--- especificacoes/features/android/screens/quadro_de_horarios_screen.rb
+++ especificacoes/features/android/screens/quadro_de_horarios_screen.rb
@@ -3,10 +3,26 @@ class QuadroDeHorariosScreen < AndroidScreenBase
   trait(:trait)                 { "* id:'#{layout_name}'" }
 
   # Declare todos os elementos da tela
-  element(:layout_name)         { 'timesheet_activity' }
+  element(:layout_name)           { 'timesheet_activity' }
+  element(:combo_time_table_type) {''}
+  element(:label_online_courses)  {'Disciplinas Online'}
 
-  def schedule_is_on_page?
-    is_on_page? "MODELAGEM DE SISTEMAS"
+  def validate_course_is_on_page(course)
+    is_on_page? course
+  end
+
+  def touch_combo_time_table_type)
+    touch("* id:'#{combo_time_table_type}'")
+  end
+
+  action(:touch_online_courses){
+    touch("* id:'Disciplinas Online'")
+  }
+
+  def validate_only_online
+  end
+
+  def validate_only_presential
   end
 
   def validate_no_schedule_available
