b1df57e279b11766ae6e0b2158d4e481a7e62b38
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-03-14 20:11:43 -0300

    updating absense assertions

--- especificacoes/features/ios/screens/faltas_screen.rb
+++ especificacoes/features/ios/screens/faltas_screen.rb
@@ -1,21 +1,20 @@
 class FaltasScreen < IOSScreenBase
   # The screen identificator
-  # trait(:trait)                 { "* marked:'#{layout_name}'" }
+  trait(:trait)                 { "* marked:'#{layout_name}'" }
 
-  element(:layout_name)         { '' }
+  element(:layout_name)         { 'absenseView' }
 
   def attendance_is_on_page?
-    is_on_page? "ARQUITETURA DE SISTEMAS DISTRIBUIDOS"
-    is_on_page? "% Faltas"
+    is_on_page? "ARQUITETURA DE SISTEMAS DISTRIBUÍDOS"
+    is_on_page? "0%"
     is_on_page? "44h"
   end
 
   def attendance_is_not_on_page?
-    is_on_page?("Total de faltas",false)
-    is_on_page?("Limite de faltas permitidas",false)
+    is_on_page?("Faltas",false)
   end
 
   def validate_no_attendance_message
-    is_on_page? "Não existe frequência para o aluno!"
+    is_on_page? "Você não está matriculado em nenhuma disciplina."
   end
 end
