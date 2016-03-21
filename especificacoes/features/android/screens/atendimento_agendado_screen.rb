b20a5815dbb4ac5dc9ac0c07e4110f481f5ceca5
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-03-21 14:08:25 -0300

    updating wrong method implementation

--- especificacoes/features/android/screens/atendimento_agendado_screen.rb
+++ especificacoes/features/android/screens/atendimento_agendado_screen.rb
@@ -14,48 +14,48 @@ class AtendimentoAgendadoScreen < AndroidScreenBase
   element(:type_name)              {''}
   element(:cause_name)             {''}
 
-  def touch_new_service_button do
+  def touch_new_service_button
     touch("* id:'#{new_service_button}'")
   end
-  def choose_category do
+  def choose_category
     touch("* id:'#{spinner_category}'")
     touch("* marked:'#{category_name}'")
   end
-  def choose_type do
+  def choose_type
     touch("* id:'#{spinner_type}'")
     touch("* marked:'#{type_name}'")
   end
-  def choose_cause do
+  def choose_cause
     touch("* id:'#{spinner_cause}'")
     touch("* marked:'#{cause_name}'")
   end
-  def choose_existing_category_type_cause do
+  def choose_existing_category_type_cause
 
   end
-  def touch_next_button do
+  def touch_next_button
     touch("* id:'#{next_button}'")
   end
-  def touch_available_hour_button do
+  def touch_available_hour_button
     touch("* id:'#{available_hour_button}'")
   end
-  def validate_choosen_informations_is_on_page do
+  def validate_choosen_informations_is_on_page
     is_on_page? cause_name
     is_on_page? type_name
     is_on_page? cause_name
   end
-  def fill_observation_field do
+  def fill_observation_field
     enter "Obs etc 154325", observation_field
   end
-  def touch_confirmation_button do
+  def touch_confirmation_button
     touch("* id:'#{confirmation_button}'")
   end
-  def validate_confirmation_message_is_on_page do
+  def validate_confirmation_message_is_on_page
     is_on_page? "Agendamento realizado com sucesso."
   end
-  def validate_existing_service_message_is_on_page do
+  def validate_existing_service_message_is_on_page
     is_on_page? "Você ja possui um agendamento previsto para o mesmo assunto"
   end
-  def validate_schedule_limite_message_is_on_page do
+  def validate_schedule_limite_message_is_on_page
     is_on_page? "Não é possível agendar um novo agendamento"
   end
 end
