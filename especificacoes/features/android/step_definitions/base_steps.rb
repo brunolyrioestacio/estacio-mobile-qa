f869bca28a0773daac7be12d584a274448c5aae8
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-04-13 18:37:05 -0300

    feeding base steps with new registration type

--- especificacoes/features/android/step_definitions/base_steps.rb
+++ especificacoes/features/android/step_definitions/base_steps.rb
@@ -1,6 +1,9 @@
 M = Hash["exclusivamente presencial"=>"201502468361","flex"=>"201401359558",
         "exclusivamente de EAD"=>"201407212771","que está fora"=>"201402031831",
-        "sem pendencias de atendimento"=>"201512955639"]
+        "sem pendencias de atendimento"=>"201512955639"
+        "que não possui requerimentos"=>""
+        "que possui requerimentos"=>""
+        ]
 Dado(/^que realizei o processo de login usando uma matrícula "(.*?)" do período vigente$/) do |tipo_matricula|
   matricula = M[tipo_matricula]
   @pageLogin.enter_matricula(matricula)
