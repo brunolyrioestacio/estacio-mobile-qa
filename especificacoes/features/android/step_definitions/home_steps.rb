d39f643c0d995d994c29d9406cc92a33ee5c9afa
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-04-13 17:49:50 -0300

    creating touch requirement button automation at home steps

--- especificacoes/features/android/step_definitions/home_steps.rb
+++ especificacoes/features/android/step_definitions/home_steps.rb
@@ -13,6 +13,9 @@ end
 Quando(/^tocar no botão de atendimento agendado$/) do
   @pageHome.tocar_botao_atendimento_agendado
 end
+Quando(/^tocar no botão de requerimentos$/) do
+  @pageHome.tocar_botao_requerimentos
+end
 ###########E############
 
 
