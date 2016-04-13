3e5f39c2773e1bb0bc52ddec9115289a2fa86439
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-04-13 18:34:07 -0300

    updating methods implementation and feature

--- especificacoes/features/android/screens/requerimento_screen.rb
+++ especificacoes/features/android/screens/requerimento_screen.rb
@@ -1,11 +1,15 @@
-class RequerimentoScreen < AndroidScreenBase
-  # Identificador da tela
-  # trait(:trait)                 { "* id:'#{layout_name}'" }
+class RequerimentosScreen < AndroidScreenBase
+  trait(:trait)                 { "* id:'#{layout_name}'" }
 
-  # Declare todos os elementos da tela
-  # element(:layout_name)         { 'insert_layout_identificator' }
+  element(:layout_name)         { 'requirement_view' }
   # element(:button)              { 'insert_button_identificator' }
 
+  def validate_no_requeriments_message
+    is_on_page? "Não existem requerimentos para consulta."
+  end
+  def validate_requirements_is_on_page
+    is_on_page? "SITUAÇÃO"
+  end
   # Declare todas as acoes da tela
   # action(:touch_button) {
   #   touch("* id:'#{button}'")
--- especificacoes/features/android/step_definitions/requerimento_steps.rb
+++ especificacoes/features/android/step_definitions/requerimento_steps.rb
@@ -1,5 +1,15 @@
 ######### DADO #########
+Dado(/^que estou na tela de Requerimentos$/) do
+  @pageRequerimentos = page(RequerimentosScreen).await(timeout: 5)
+end
 
 ######### QUANDO #########
 
 ######### ENTãO #########
+Então(/^devo ver uma mensagem sobre a falta de requerimentos existentes abertos ou concluídos$/) do
+  @pageRequerimentos.validate_no_requeriments_message
+end
+
+Então(/^devo ver uma lista de requerimentos existentes abertos ou concluídos$/) do
+  @pageRequerimentos.validate_requirements_is_on_page
+end
