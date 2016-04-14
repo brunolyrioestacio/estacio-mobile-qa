f20295d6a2f0c48843a136dfa486b0eadba8f9b9
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-04-14 16:56:27 -0300

    fixing wrong string definition on registration map at base steps

--- especificacoes/features/android/step_definitions/base_steps.rb
+++ especificacoes/features/android/step_definitions/base_steps.rb
@@ -1,8 +1,8 @@
 M = Hash["exclusivamente presencial"=>"201502468361","flex"=>"201401359558",
         "exclusivamente de EAD"=>"201407212771","que está fora"=>"201402031831",
-        "sem pendencias de atendimento"=>"201512955639"
-        "que não possui requerimentos"=>""
-        "que possui requerimentos"=>""
+        "sem pendencias de atendimento"=>"201512955639",
+        "que não possui requerimentos"=>"",
+        "que possui requerimentos"=>"201401359558"
         ]
 Dado(/^que realizei o processo de login usando uma matrícula "(.*?)" do período vigente$/) do |tipo_matricula|
   matricula = M[tipo_matricula]
