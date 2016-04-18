da11e81649501261fb72e6ae8a43e393024597c8
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-04-18 20:45:56 -0300

    Fix wrong method name at scheduled service scenario

--- especificacoes/features/android/screens/atendimento_agendado_screen.rb
+++ especificacoes/features/android/screens/atendimento_agendado_screen.rb
@@ -13,7 +13,7 @@ class AtendimentoAgendadoScreen < AndroidScreenBase
   element(:detail_button)               {'detail_btn'}
   element(:review_buttonn)              {'review_btn'}
   element(:cancel_button)               {'cancel_btn'}
-  element(:reschedule_button)           {'reschedule_btn'}
+  element(:reschedule_button)           {'rescheduled_btn'}
   element(:cancel_confirmation_button)  {'Sim'}
   element(:bad_option_radio_button)     {'bad_option'}
   element(:category_name)               {'ENTREGA E RETIRADA DE DOCUMENTO/MATERIAL DIDÁTICO'}
@@ -23,8 +23,8 @@ class AtendimentoAgendadoScreen < AndroidScreenBase
   def validate_review_screen_is_on_page?
     pending
   end
-  def touch_reschedule_buttontouch_reschedule_buttontouch_reschedule_buttontouch_reschedule_button
-    touch("* id:'#{reschedule_btn}'")
+  def touch_reschedule_button
+    touch("* id:'#{reschedule_button}'")
   end
   def touch_bad_option
     touch("* id:'#{bad_option_radio_button}'")
@@ -64,7 +64,7 @@ class AtendimentoAgendadoScreen < AndroidScreenBase
     touch("* marked:'#{next_button}'")
   end
   def touch_available_hour_button
-    touch("* id:'#{available_hour_button}'")
+    touch("* id:'#{available_hour_button}' index:4")
   end
   def validate_choosen_informations_is_on_page
     is_on_page? cause_name
