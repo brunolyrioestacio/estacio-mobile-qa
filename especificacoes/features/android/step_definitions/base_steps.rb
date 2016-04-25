0bdc6b81b7fc5da424cf6b2b1d51c687f97ca9ed
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-04-25 17:20:15 -0300

    adding a new registration to registration hash map

--- especificacoes/features/android/step_definitions/base_steps.rb
+++ especificacoes/features/android/step_definitions/base_steps.rb
@@ -2,7 +2,8 @@ M = Hash["exclusivamente presencial"=>"201502468361","flex"=>"201401359558",
         "exclusivamente de EAD"=>"201407212771","que está fora"=>"201402031831",
         "sem pendencias de atendimento"=>"200301489494",
         "que não possui requerimentos"=>"",
-        "que possui requerimentos"=>"201401359558"
+        "que possui requerimentos"=>"201401359558",
+        "que possui aulas aos domingos"=>"201312054001"
         ]
 Dado(/^que realizei o processo de login usando uma matrícula "(.*?)" do período vigente$/) do |tipo_matricula|
   matricula = M[tipo_matricula]
