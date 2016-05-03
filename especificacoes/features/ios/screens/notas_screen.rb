458aefbf3281a670b0274b374e499b3979ee91a1
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-05-03 20:19:44 -0300

    Updating grades validation at ios and android screens

--- especificacoes/features/ios/screens/notas_screen.rb
+++ especificacoes/features/ios/screens/notas_screen.rb
@@ -6,17 +6,18 @@ class NotasScreen < IOSScreenBase
   element(:layout_name)         { 'gradesView' }
 
   def grades_is_on_page?
-    is_on_page? "Estrutura de dados"
-    is_on_page? "3.0" #AV1#
-    is_on_page? "8.0" #AV2#
-    is_on_page? "9.0" #AV3#
+    is_on_page? 'Estrutura de dados'
+    is_on_page? '3.9' # AV1
+    is_on_page? '7.0' # AV2
+    is_on_page? '5.9' # AV3
+    is_on_page? '6.0' # AVR
   end
 
   def validate_no_grades
-    is_on_page? "Não existem lançamentos de notas para você no Período Acadêmico vigente!"
+    is_on_page? 'Não existem lançamentos de notas para você no Período Acadêmico vigente!'
   end
 
   def validate_no_registration
-    is_on_page? "Aluno não está matriculado em nenhum Período Acadêmico vigente."
+    is_on_page? 'Aluno não está matriculado em nenhum Período Acadêmico vigente.'
   end
 end
