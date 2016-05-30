40f3ff1c5eef62b6d82a0f34a199f2a6a79ef1bb
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-05-30 17:39:23 -0300

    Revert "pre implementing complaint steps"
    This reverts commit 7ceb7da43622553c4d0aeb4c530fec9feae34c44.

--- especificacoes/features/android/step_definitions/requerimento_steps.rb
+++ especificacoes/features/android/step_definitions/requerimento_steps.rb
@@ -16,20 +16,12 @@ Quando(/^tocar no botão flutuante de Ouvidoria$/) do
   @page_requerimentos.touch_new_ombudsman_float_button
 end
 
-Quando(/^tocar no botão flutuante de Reclamação$/) do
-  @page_requerimentos.touch_new_complaint_float_button
-end
-
 Quando(/^escolher uma categoria$/) do
   @page_requerimentos.choose_categoty
 end
 
 Quando(/^escolher um tipo de ouvidoria$/) do
   @page_requerimentos.choose_ombudsman_type
-end
-
-Quando(/^escolher um tipo de reclamação$/) do
-  @page_requerimentos.choose_complaint_type
 end
 
 Quando(/^um tipo de requerimento$/) do
@@ -46,11 +38,6 @@ Quando(/^tocar em próximo para validar as informações do tipo de ouvidoria es
   @page_requerimentos.validate_ombudsman_information_is_on_page
 end
 
-Quando(/^tocar em próximo para validar as informações do tipo de reclamação escolhida$/) do
-  @page_requerimentos.touch_next_button
-  @page_requerimentos.validate_complaint_information_is_on_page
-end
-
 Quando(/^tocar em próximo novamente$/) do
   @page_requerimentos.touch_next_button
 end
@@ -63,20 +50,12 @@ Quando(/^preencher o campo de observação com informações sobre a ouvidoria$/
   @page_requerimentos.fill_obsersation_field
 end
 
-Quando(/^preencher o campo de observação com informações sobre a reclamação$/) do
-  @page_requerimentos.fill_obsersation_field
-end
-
 Quando(/^tocar em finalizar para abrir o requerimento$/) do
   @page_requerimentos.touch_finish_requirement_button
 end
 
 Quando(/^tocar em finalizar para abrir o requerimento de ouvidoria$/) do
   @page_requerimentos.touch_finish_ombudsman_button
-end
-
-Quando(/^tocar em finalizar para abrir o requerimento de reclamação$/) do
-  @page_requerimentos.touch_finish_complaint_button
 end
 
 ######### ENTAO #########
@@ -94,8 +73,4 @@ end
 
 Então(/^devo ver uma mensagem de confirmação da criação de Ouvidoria$/) do
   @page_requerimentos.validate_ombudsman_confirmation_message_is_on_page
-end
-
-Então(/^devo ver uma mensagem de confirmação da criação de Reclamação$/) do
-  @page_requerimentos.validate_complaint_confirmation_message_is_on_page
 end
