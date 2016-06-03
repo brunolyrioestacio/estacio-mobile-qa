af20ef81716698e99b7935d164d8a786a7f646da
Natan Ximenes <natan.ximenes@CSRJL051M.concrete.com>
2016-06-03 17:04:51 -0300

    Updating steps implementation

--- especificacoes/features/android/step_definitions/push_steps.rb
+++ especificacoes/features/android/step_definitions/push_steps.rb
@@ -1,19 +1,20 @@
+n = rand(0..1000)
 ######### DADO #########
 Dado(/^que enviei uma mensagem Push para devices Android$/) do
-  @pageHome.send_push
+  @pageHome.send_push(n)
 end
 
 ######### QUANDO #########
 Quando(/^tocar no botão de Notificações$/) do
-  @pageHome.touch_notifications_buttons
+  @pageHome.touch_notifications_button
 end
 
 ######### ENTAO #########
 
 Então(/^devo estar na tela de Notificações$/) do
-  @page_notifications = page(PushScreen).wait(timeout: 5)
+  @page_notifications = page(PushScreen).await(timeout: 5)
 end
 
 Então(/^devo ver a mensagem Push enviada previamente$/) do
-  @page_notifications.validate_push_message_is_on_page
+  @page_notifications.validate_push_message_is_on_page(n)
 end
