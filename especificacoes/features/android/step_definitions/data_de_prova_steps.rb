10b6a7b0cbebf14cc4de579f214017dd37fe9158
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-06-10 19:28:20 -0300

    Pre implementing android exam scheduling steps

---
+++ especificacoes/features/android/step_definitions/data_de_prova_steps.rb
@@ -0,0 +1,29 @@
+############ Dado #############
+Dado(/^que estou na tela de data de prova$/) do
+  @page_data = page(DataDeProvaScreen).await(timeout: 5)
+end
+
+Dado(/^toquei no botão de datas de provas online$/) do
+  @page_data.tocar_botao_data_online
+end
+
+############ Quando ###########
+
+Quando(/^tocar no botão de data de prova$/) do
+  @pageHome.tocar_botao_data_de_prova
+end
+
+############ E ############
+
+############ Então ###########
+Então(/^devo vizualizar a data das provas das disciplinas presenciais$/) do
+  @page_data.validate_presential_exam_date_is_on_page
+end
+
+Então(/^devo ver uma mensagem de não há provas agendadas$/) do
+  @page_data.validate_no_exam_dates_exception_message
+end
+
+Então(/^devo vizualizar a data das provas das disciplinas online$/) do
+  @page_data.validate_online_exam_date_is_on_page
+end
