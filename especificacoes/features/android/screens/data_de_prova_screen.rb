e18eb625f02fdc116923f3f2df2cc68365246072
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-06-10 19:38:53 -0300

    implementing android exam scheduling methods even without ids

---
+++ especificacoes/features/android/screens/data_de_prova_screen.rb
@@ -0,0 +1,22 @@
+class DataDeProvaScreen < AndroidScreenBase
+  trait(:trait)                 { "* marked:'#{layout_name}'" }
+
+  element(:layout_name)         { '' }
+  element(:online_button)       { '' }
+
+  def tocar_botao_data_online
+    touch("* marked:'#{online_button}'")
+  end
+
+  def validate_presential_exam_date_is_on_page
+    is_on_page? 'ESTRUTURA DE DADOS'
+  end
+
+  def validate_online_exam_date_is_on_page
+    is_on_page? 'Projeto de design editorial'
+  end
+
+  def validate_no_exam_dates_exception_message
+    is_on_page? 'Sem provas'
+  end
+end
