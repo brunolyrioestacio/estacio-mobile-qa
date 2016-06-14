4b06450bd96a2e9fa58c74d2263c4450618bb019
Natan Ximenes <natan.ximenes@CSRJL051M.local>
2016-06-14 19:23:32 -0300

    Removing unnecessary scenarios that behave in a indeterministc way at Requerimento feature

--- especificacoes/features/android/step_definitions/requerimento_steps.rb
+++ especificacoes/features/android/step_definitions/requerimento_steps.rb
@@ -12,18 +12,9 @@ Quando(/^tocar no botão flutuante de Requerimento$/) do
   @page_requerimentos.touch_new_requirement_float_button
 end
 
-Quando(/^tocar no botão flutuante de Ouvidoria$/) do
-  @page_requerimentos.touch_new_ombudsman_float_button
-end
-
 Quando(/^escolher uma categoria$/) do
   @page_requerimentos.choose_categoty
 end
-
-Quando(/^escolher um tipo de ouvidoria$/) do
-  @page_requerimentos.choose_ombudsman_type
-end
-
 Quando(/^um tipo de requerimento$/) do
   @page_requerimentos.choose_requirement_type
 end
@@ -31,11 +22,6 @@ end
 Quando(/^tocar em próximo para validar as informações do requerimento escolhido$/) do
   @page_requerimentos.touch_next_button
   @page_requerimentos.validate_requirements_information_is_on_page
-end
-
-Quando(/^tocar em próximo para validar as informações do tipo de ouvidoria escolhida$/) do
-  @page_requerimentos.touch_next_button
-  @page_requerimentos.validate_ombudsman_information_is_on_page
 end
 
 Quando(/^tocar em próximo novamente$/) do
@@ -46,16 +32,8 @@ Quando(/^preencher o campo de observação com informações sobre o requeriment
   @page_requerimentos.fill_obsersation_field
 end
 
-Quando(/^preencher o campo de observação com informações sobre a ouvidoria$/) do
-  @page_requerimentos.fill_obsersation_field
-end
-
 Quando(/^tocar em finalizar para abrir o requerimento$/) do
   @page_requerimentos.touch_finish_requirement_button
-end
-
-Quando(/^tocar em finalizar para abrir o requerimento de ouvidoria$/) do
-  @page_requerimentos.touch_finish_ombudsman_button
 end
 
 ######### ENTAO #########
@@ -69,8 +47,4 @@ end
 
 Então(/^devo ver uma mensagem de confirmação da criação de Requerimento$/) do
   @page_requerimentos.validate_confirmation_message_is_on_page
-end
-
-Então(/^devo ver uma mensagem de confirmação da criação de Ouvidoria$/) do
-  @page_requerimentos.validate_ombudsman_confirmation_message_is_on_page
 end
