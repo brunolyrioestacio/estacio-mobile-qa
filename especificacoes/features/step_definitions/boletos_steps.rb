c4a5081c6a7a14a7623cefe9f7b27c1b835b270b
Natan Ximenes <natan.ximenes@concretesolutions.com.br>
2016-07-08 12:08:28 -0300

    Sprint 12 - Gerando implementação dos steps de boleto

--- especificacoes/features/step_definitions/boletos_steps.rb
+++ especificacoes/features/step_definitions/boletos_steps.rb
@@ -1,5 +1,46 @@
 ######### DADO #########
 
 ######### QUANDO #########
+Quando(/^navegar até a funcionalidade de boletos$/) do
+  pending # express the regexp above with the code you wish you had
+end
 
-######### ENTãO #########
+Quando(/^clicar na aba de boletos pendentes$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^clicar na aba de boletos não pendentes$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^escolher um dos boletos pendentes e navegas até sua opção de segunda via$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^clicar no botão de código de barras$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^preencher o campo de email opcional, visto que não tenho email principal$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^clicar no botão de envio por email$/) do
+  pending # express the regexp above with the code you wish you had
+end
+######### ENTAO #########
+Então(/^devo visualizar uma mensagem sobre a falta de boletos pendentes$/) do
+  pending # express the regexp above with the code you wish you had
+end
+Então(/^devo visualizar uma mensagem sobre a falta de boletos não pendentes$/) do
+  pending # express the regexp above with the code you wish you had
+end
+Então(/^devo visualizar boletos dos dois tipos$/) do
+  pending # express the regexp above with the code you wish you had
+end
+Então(/^devo visualizar uma mensagem de confirmação da cópia$/) do
+  pending # express the regexp above with the code you wish you had
+end
+Então(/^devo visualizar uma mensagem de confirmação de email enviado$/) do
+  pending # express the regexp above with the code you wish you had
+end
