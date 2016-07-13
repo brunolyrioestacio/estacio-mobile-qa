a0f06c608fc96035da8f46194d847dce0245a952
Natan Ximenes <natan.ximenes@concretesolutions.com.br>
2016-07-13 16:53:31 -0300

    Sprint 12 - Atualizando métodos e acertando timeouts da tela de boleto no ios

--- especificacoes/features/ios/screens/boletos_screen.rb
+++ especificacoes/features/ios/screens/boletos_screen.rb
@@ -2,16 +2,16 @@ class BoletosScreen < IOSScreenBase
   # Identificador da tela
   trait(:trait)                     { "* id:'#{layout_name}'" }
 
-  element(:layout_name)             { '' }
-  element(:bank_slip_item)          { '' }
-  element(:copy_button)             { '' }
-  element(:send_email_button)       { '' }
-  element(:second_bank_slip_button) { '' }
-  element(:optional_email_field)    { '' }
-  element(:pending_tab)             { '' }
-  element(:not_pending_tab)         { '' }
-  element(:primary_email_label)     { '' }
-
+  element(:layout_name)             { 'invoiceTabView' }
+  element(:bank_slip_item)          { 'invoiceHeaderArrow' }
+  element(:copy_button)             { 'invoiceDetailCopyButton' }
+  element(:send_email_button)       { 'invoiceDetailSendButton' }
+  element(:second_bank_slip_button) { 'invoiceOpenDetail' }
+  element(:optional_email_field)    { 'invoiceDetailOptionalEmail' }
+  element(:pending_tab)             { 'Pendentes' }
+  element(:not_pending_tab)         { 'Não Pendentes' }
+  element(:primary_email_label)     { 'Principal:' }
+  element(:view_to_hide_keyboard)   { 'invoiceDetailOrLabel' }
   OPTIONAL_EMAIL = 'meupai.omeupai@gmail.com'.freeze
   NO_PENDING_BANK_SLIPS_MESSAGE = 'Você não possui nenhum boleto em processamento, com pagamento pendente ou vencido para consulta.'.freeze
   NO_NOT_PENDING_BANK_SLIPS_MESSAGE = 'Você não possui nenhum boleto pago, negociado, bloqueado ou cancelado para consulta.'.freeze
@@ -19,25 +19,32 @@ class BoletosScreen < IOSScreenBase
   SENT_EMAIL_MESSAGE = 'Boleto enviado com sucesso'.freeze
 
   def touch_pending_bank_slips
+    sleep 2
     touch("* marked:'#{pending_tab}'")
   end
 
   def touch_not_pending_bank_slips
+    sleep 2
     touch("* marked:'#{not_pending_tab}'")
   end
 
   def touch_pending_bank_slip_item_and_go_to_second_copy
+    sleep 2
     touch("* id:'#{bank_slip_item}'")
+    sleep 1
     touch("* id:'#{second_bank_slip_button}'")
   end
 
   def touch_copy_button
+    sleep 1
     touch("* id:'#{copy_button}'")
   end
 
   def fill_optional_email_field_and_validate_no_primary_email_is_on_page
+    sleep 3
     is_on_page?(primary_email_label, false)
     enter OPTIONAL_EMAIL, optional_email_field
+    touch("* marked:'#{view_to_hide_keyboard}'")
   end
 
   def touch_send_email_button
@@ -48,6 +55,7 @@ class BoletosScreen < IOSScreenBase
   def fill_optional_email_field
     sleep 2
     enter OPTIONAL_EMAIL, optional_email_field
+    touch("* marked:'#{view_to_hide_keyboard}'")
   end
 
   def validate_no_pending_bank_slips_message_is_on_page
@@ -71,6 +79,7 @@ class BoletosScreen < IOSScreenBase
   end
 
   def validate_sent_email_message_is_on_page
+    sleep 3
     is_on_page? SENT_EMAIL_MESSAGE
   end
 end
