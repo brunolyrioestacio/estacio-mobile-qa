8d211434fc6bf608a2a9a5d20d7c47b0e514b5b4
Natan Ximenes <natan.ximenes@concretesolutions.com.br>
2016-07-20 18:39:19 -0300

    Sprint 13 - Adequando a funcionalidade iOS de Datas de Provas para ser mais imperativa e menos imperativa

--- especificacoes/features/ios/step_definitions/data_de_prova_steps.rb
+++ especificacoes/features/ios/step_definitions/data_de_prova_steps.rb
@@ -1,30 +1,21 @@
 ############ Dado #############
-Dado(/^que estou na tela de data de prova$/) do
-  @pageData = page(DataDeProvaScreen).await(timeout: 5)
-
+Quando(/^tocar no botão de datas de provas online$/) do
+  @page_data.tocar_botao_data_online
 end
 
-Dado(/^toquei no botão de datas de provas online$/) do
-  @pageData.tocar_botao_data_online
+############ Entao ###########
+Então(/^devo vizualizar as datas das provas das disciplinas presenciais$/) do
+  @page_data.validate_presential_exam_date_is_on_page
 end
 
-############ Quando ###########
-
-Quando(/^tocar no botão de data de prova$/) do
-  @pageHome.tocar_botao_data_de_prova
+Então(/^devo ver uma mensagem sobre a não existencia de provas online marcadas$/) do
+  @page_data.validate_no_exam_dates_exception_message
 end
 
-############ E ############
-
-############ Então ###########
-Então(/^devo vizualizar a data das provas das disciplinas presenciais$/) do
-  @pageData.validate_presential_exam_date_is_on_page
+Então(/^devo ver uma mensagem sobre a não existencia de provas presenciais marcadas$/) do
+  @page_data.validate_no_exam_dates_exception_message
 end
 
-Então(/^devo ver uma mensagem de não há provas agendadas$/) do
-  @pageData.validate_no_exam_dates_exception_message
-end
-
-Então(/^devo vizualizar a data das provas das disciplinas online$/) do
-  @pageData.validate_online_exam_date_is_on_page
+Então(/^devo vizualizar as datas das provas das disciplinas online$/) do
+  @page_data.validate_online_exam_date_is_on_page
 end
